7d169550ad3c02c7827e251aa781849ad791411c
amarukawanobiru <k4tsu0502@gmail.com>
2023-10-31 01:06:22 +0900

    🚧 iOS2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,21 +5,7 @@ const setFillHeight = () => {
     "--view-height",
     `${viewHeight}px`
   );
-
-  const drawerMenuHeiht = $drawerMenu.scrollHeight;
-  document.documentElement.style.setProperty(
-    "--drawerMenu-height",
-    `${drawerMenuHeiht}px`
-  );
 };
-
-window.addEventListener("load", () => {
-  setFillHeight();
-});
-
-window.addEventListener("resize", () => {
-  setFillHeight();
-});
 
 // ***** ***** ***** Hamburger menu
 const $hamburger = document.querySelector(".hamburger");
@@ -28,4 +14,18 @@ const $drawerMenu = document.querySelector(".drawer-menu");
 $hamburger.addEventListener("click", () => {
   $hamburger.classList.toggle("js-active");
   $drawerMenu.classList.toggle("js-active");
+});
+
+window.addEventListener("load", () => {
+  setFillHeight();
+
+  const drawerMenuHeiht = $drawerMenu.scrollHeight;
+  document.documentElement.style.setProperty(
+    "--drawerMenu-height",
+    `${drawerMenuHeiht}px`
+  );
+});
+
+window.addEventListener("resize", () => {
+  setFillHeight();
 });
